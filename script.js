a367c4bc326d96a4bd5f395084903b3c1e56662c
Abhinandana <bollempallyabhinandana@gmail.com>
2026-02-23 11:29:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,13 @@ if(caseData.advocates){
     Query processing time: ${lag} ms
   `;
 }
+function startSearch(){
+  const query = document.getElementById("searchInput").value;
+  localStorage.setItem("sealaw_query", query);
+  location.href="results.html";
+}
+
+function quickSearch(q){
+  localStorage.setItem("sealaw_query", q);
+  location.href="results.html";
+}
