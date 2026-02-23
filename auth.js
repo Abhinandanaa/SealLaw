cc1105579857821bb498c2b91474e33234715199
Abhinandana <bollempallyabhinandana@gmail.com>
2026-02-23 11:30:40 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -29,3 +29,19 @@ function goAdmin(){
     alert("Admin access only");
   }
 }
+function openAuth(type){
+  const email=prompt("Enter email:");
+  const password=prompt("Enter password:");
+  
+  if(type==="signup"){
+    localStorage.setItem("sealaw_user",JSON.stringify({email,password,role:"user"}));
+    alert("Account created");
+  }else{
+    const user=JSON.parse(localStorage.getItem("sealaw_user"));
+    if(user && user.email===email && user.password===password){
+      alert("Login successful");
+    }else{
+      alert("Invalid credentials");
+    }
+  }
+}
